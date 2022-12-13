5bbf90be5a0c4c7e52db301c1a7d3cce2e338b62
helongjun <lanrenspace@163.com>
2022-12-13 11:37:38 +0800

    init commit

---
+++ src/test/java/com/lanrenspace/assistant/lottery/LotteryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lanrenspace.assistant.lottery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LotteryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
